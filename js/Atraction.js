88b9f9384db3bc8e6bcb93ec68508c9b3bb8faae
Leart-O <leartobertinca6@gmail.com>
2024-11-24 15:00:09 +0100

    ...

--- js/Atraction.js
+++
@@ -1,7 +0,0 @@
-function changeButtonText(newText) {
-    document.querySelector('.dropbtn').innerText = newText;
-  }
-  
-  function changeButtonText2(newText2) {
-    document.querySelector('.dropbtn2').innerText = newText2;
-  }
